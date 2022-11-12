59103cc88a28fdae96225969153738ef97a1eed6
Szigetvari Simon <szigetvari.simon@gmail.com>
2022-11-12 17:30:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/holkonnyitsek/WcInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.holkonnyitsek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WcInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wc_info)
+    }
+}

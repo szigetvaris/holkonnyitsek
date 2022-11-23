47c9a7f96542503b4e489f09b5618fc56935bf20
BenceSzollosi <33355817+BenceSzollosi@users.noreply.github.com>
2022-11-23 16:49:04 +0100

    update location

--- app/src/main/java/com/example/holkonnyitsek/MapsActivity.kt
+++ app/src/main/java/com/example/holkonnyitsek/MapsActivity.kt
@@ -53,8 +53,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val bmeI = LatLng(47.4726408, 19.0583993)
+        mMap.addMarker(MarkerOptions().position(bmeI).title("Marker in BME I building"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bmeI,17f))
     }
 }
